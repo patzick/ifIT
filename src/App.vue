58bd6a60bd440fbbdfba31e3128abf279bb7af63
patzick <tomczyk.patryk@gmail.com>
2019-10-24 23:01:28 +0200

    chore: move new files to old project

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
-<template lang="pug">
-  #app
-    head-bar
-    main-content Development done right.
-    social-links
+<template>
+  <div id="app">
+    <head-bar />
+    <router-view />
+  </div>
 </template>
 
 <script>
 import HeadBar from "./components/HeadBar.vue";
-import MainContent from "./components/MainContent.vue";
-import SocialLinks from "./components/SocialLinks.vue";
 
 export default {
   components: {
-    HeadBar,
-    MainContent,
-    SocialLinks
+    HeadBar
   }
 };
 </script>
 
-<style lang="sass">
-#app
-  font-family: "Avenir", Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  color: #2A3D45
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2a3d45;
+}
 
-body
-  margin: 0
+body {
+  margin: 0;
+}
 </style>
--- src/components/SocialLinks.vue
+++ src/components/SocialLinks.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="social-links">
     <a href="https://github.com/patzick" target="_blank" class="social-link">
-        <img class="social-link-icon" alt="GitHub" src="../assets/github.svg" />
-        <div class="social-link-title">patzick</div>
+      <img class="social-link-icon" alt="GitHub" src="../assets/github.svg" />
+      <div class="social-link-title">patzick</div>
     </a>
-    <a href="https://www.linkedin.com/in/patryk-tomczyk-998763b3/" target="_blank" class="social-link">
-        <img class="social-link-icon" alt="LinkedIn" src="../assets/linkedin.svg" />
-        <div class="social-link-title">Patryk Tomczyk</div>
+    <a
+      href="https://www.linkedin.com/in/patryk-tomczyk-998763b3/"
+      target="_blank"
+      class="social-link"
+    >
+      <img
+        class="social-link-icon"
+        alt="LinkedIn"
+        src="../assets/linkedin.svg"
+      />
+      <div class="social-link-title">Patryk Tomczyk</div>
     </a>
     <a href="mailto:contact@ifit.pt" class="social-link">
-        <img class="social-link-icon" alt="GitHub" src="../assets/mail.svg" />
-        <div class="social-link-title">contact@ifit.pt</div>
+      <img class="social-link-icon" alt="GitHub" src="../assets/mail.svg" />
+      <div class="social-link-title">contact@ifit.pt</div>
     </a>
   </div>
 </template>
